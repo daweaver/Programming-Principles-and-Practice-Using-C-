098a16d9897ab7349e0bd1000401eb529634ce50
Devere A. Weaver <weaver_dev@protonmail.com>
2020-05-04 09:17:05 -0500

    Update chapter 8 exercises

--- Ch.8 - Techincalities/ex7_swap_arrays/main.cpp
+++ Ch.8 - Techincalities/ex7_swap_arrays/main.cpp
@@ -6,12 +6,36 @@
 
 using namespace std;
 
+// void swap(vector<string> n, vector<int> &a)
+// {
+//     if (n[1] < n[0])
+//     {
+//         int temp = a[1];
+//         a[0] = a[1];
+//         a[1] = a[0];
+//     }
+//     if (n[1] > n[2])
+//}
+
+void func(vector<string> nc, vector<string> sn, vector<int> a)
+{
+    // find the index of the original name vector (nc) elements 
+    // in the sorted name vector (sn)
+    for (size_t i{0}; i < nc.size(); ++i)
+    {
+        // find nc element in sn 
+        if (nc[i] != sn[i])
+        {
+        }
+    }
+}
+
 int main()
 {
-    vector<string> name(5);
-    vector<int> age(5);
+    vector<string> name(3);     // TO DO: cange this back to 5 later
+    vector<int> age(3);
 
-    for (size_t i{0}; i < 5; ++i)
+    for (size_t i{0}; i < 3; ++i)
     {
         cout << "Enter name " << i + 1 << ": ";
         cin >> name[i];
@@ -20,9 +44,9 @@ int main()
         cout << '\n';
     }
 
-    for (size_t i{0}; i < 5; ++i)       // print out vectors, put into function 
+    for (size_t i{0}; i < 3; ++i)       // print out vectors, put into function 
     {
-        if (i == 4)
+        if (i == 2)
             cout << '(' << name[i] << ", " << age[i] << ")\n";
         else
 
@@ -31,15 +55,14 @@ int main()
 
     // make copy of names vector before sorting it 
     vector<string> name_copy = name;
-
     sort(name.begin(), name.end());
 
     // TO DO: Figure out how to sort the age vector to 
     // match the names vector values 
 
-    for (size_t i{0}; i < 5; ++i)       // print out vectors, put into function 
+    for (size_t i{0}; i < 3; ++i)       // print out vectors, put into function 
     {
-        if (i == 4)
+        if (i == 2)
             cout << '(' << name[i] << ", " << age[i] << ")\n";
         else
 
